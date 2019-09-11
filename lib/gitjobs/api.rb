e93d7817df8fb45bd8e8796cf32bedef18708702
Shaun <ycorredius@gmail.com>
2019-09-11 17:13:01 -0400

    Working Api

--- lib/gitjobs/api.rb
+++ lib/gitjobs/api.rb
@@ -6,16 +6,28 @@ class Api
     attr_reader :posts
     include HTTParty
     # binding.pry
-
+    @@all 
     def initialize
         @posts = []
     end
 
-    def self.pull_post
+    def self.save 
+        @@all << @posts
+    end
+
+    def pull_post
         response = HTTParty.get('https://jobs.github.com/positions.json?description=ruby&page=1',format: :json)
         response.each do |post| 
-            binding.pry
+            listing = {
+                title: post['title'],                    
+                company: post["company"],
+                location: post['location'],
+                description: post['description'],
+                url: post['company_url']
+            }
+            @posts << listing
         end
+        binding.pry
    end
 end
 
